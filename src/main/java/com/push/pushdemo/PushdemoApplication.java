8208d86068c92f3b97f1a089fa622c4517417414
Armand Gaillard <armandgaillard@armands-air.home>
2021-03-08 16:01:23 +0000

    first commit

---
+++ src/main/java/com/push/pushdemo/PushdemoApplication.java
@@ -0,0 +1,13 @@
+package com.push.pushdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/push/pushdemo/PushdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.push.pushdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
